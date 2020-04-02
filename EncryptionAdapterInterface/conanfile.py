a9ac5e1c075687e76521583e22b5e42dc4e5a4ce
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-04-03 00:15:57 +0200

    Added conan recipe for encryption adapter interface

---
+++ EncryptionAdapterInterface/conanfile.py
@@ -0,0 +1,19 @@
+from conans import ConanFile
+
+
+class EncryptionAdapterInterfaceConan(ConanFile):
+    name = "EncryptionAdapterInterface"
+    description = "Interface of library-agnostic API for C++ to encrypt data"
+    url = "https://github.com/systelab/cpp-encryption-adapter"
+    homepage = "https://github.com/systelab/cpp-encryption-adapter"
+    author = "CSW <csw@werfen.com>"
+    topics = ("conan", "encryption", "adapter", "wrapper", "interface")
+    license = "MIT"
+    generators = "cmake_find_package"
+    # No settings/options are necessary, this is header only
+
+    def package(self):
+        self.copy("*.h", dst="include/EncryptionAdapterInterface")
+
+    def package_info(self):
+        self.info.header_only()
